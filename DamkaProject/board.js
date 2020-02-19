1e059274c64f49cf21472fdfcf9cc75f6b256cc7
mendi8345 <46045487+mendi8345@users.noreply.github.com>
2020-02-19 13:25:24 +0200

    Add files via upload

---
+++ DamkaProject/board.js
@@ -0,0 +1,322 @@
+var forcedKillOnBoard = false;
+var readyTokillNextMove;
+var anotherdKillForThisTurn = false;
+var numOfKillInThisTurn = 0;
+var numOfKingsMoveWithoutKill = 0;
+function createBoard() {
+    var board = document.createElement("div");
+    board.id = "board";
+    board.rows =
+        [
+            createRow(1),
+            createRow(2),
+            createRow(3),
+            createRow(4),
+            createRow(5),
+            createRow(6),
+            createRow(7),
+            createRow(8)
+        ];
+    for (var i = 0; i < board.rows.length; i++) {
+        console.log(board.rows[i].id);
+        console.log(board.rows[i]);
+        board.insertBefore(board.rows[i], board.childNodes[i]);
+    }
+    document.body.appendChild(board);
+    console.log(board.rows);
+    renderCheckers();
+    return board;
+}
+
+function createRow(rowNum) {
+    var row = document.createElement("div");
+    row.id = "row-" + rowNum;
+    row.className = "row";
+
+    row.cells =
+        [
+            createCell(rowNum, 1),
+            createCell(rowNum, 2),
+            createCell(rowNum, 3),
+            createCell(rowNum, 4),
+            createCell(rowNum, 5),
+            createCell(rowNum, 6),
+            createCell(rowNum, 7),
+            createCell(rowNum, 8)
+        ];
+
+    for (var i = 0; i < row.cells.length; i++) {
+        console.log(row.cells[i].id);
+        console.log(row.cells[i]);
+        row.insertBefore(row.cells[i], row.childNodes[i]);
+    }
+    console.log(row.cells);
+    return row;
+}
+
+function createCell(rowNum, cellNum) {
+    var cell = document.createElement("div");
+    cell.id = "cell-" + rowNum + '-' + cellNum;
+    if (cellColor(rowNum, cellNum) === 'black') {
+        cell.className = "cell black";
+    } else {
+        cell.className = "cell white";
+    }
+    return cell;
+}
+function moveSelectedCheckerHere() {
+    if (selectedChecker) {
+        var blackCell = this;
+        var id = blackCell.id;
+        var idParts = id.split('-');
+        checkerRow = selectedChecker.row;
+        checkerCell = selectedChecker.cell;
+
+        cellRow = Number(idParts[1])
+        cell = Number(idParts[2]);
+        var numOfCheckersBeforMove = checkers.length;
+
+        if (isALegalMove(selectedChecker, blackCell)) {
+            selectedChecker.row = cellRow;
+            selectedChecker.cell = cell;
+
+            if (killedCheckersPositionInArray != undefined) {
+                checkers.splice(killedCheckersPositionInArray, 1);
+                killedCheckersPositionInArray = undefined;
+            }
+
+            if (selectedChecker.color == `black` && selectedChecker.row == 1) {
+                selectedChecker.isKing = true
+            } else if (selectedChecker.color == `white` && selectedChecker.row == 8) {
+                selectedChecker.isKing = true
+            }
+            renderCheckers();
+            var numOfCheckersAfterMove = checkers.length;
+            if (numOfCheckersAfterMove != numOfCheckersBeforMove) {
+                numOfKillInThisTurn++;
+                numOfKingsMoveWithoutKill = 0;
+            }
+            if (numOfKillInThisTurn > 0 && isThereAnotherForcedKillForThisTurn(selectedChecker)) {
+            } else {
+                if (selectedChecker.isKing) {
+                    if (numOfCheckersAfterMove == numOfCheckersBeforMove) {
+                        numOfKingsMoveWithoutKill++;
+                        isDraw(numOfKingsMoveWithoutKill);
+                    }
+                } else {
+                    numOfKingsMoveWithoutKill = 0;
+                }
+                isThereAForcedKillOnBoard();
+                selectedChecker = undefined;
+                turn = turn == "white" ? "black" : "white";
+                numOfKillInThisTurn = 0;
+            }
+        }
+
+    }
+    console.log(`select a checker, foo!`)
+}
+
+function cellColor(cellNum, rowNum) {
+    return parity(cellNum) == parity(rowNum) ? 'white' : 'black'
+}
+
+/*** Helper Methods ***/
+function parity(num) {
+    return (num % 2 == 0) ? 'even' : 'odd';
+}
+
+function clearBoard(firstBoard) {
+    if (!firstBoard) {
+
+        var blackCells = document.getElementsByClassName("black cell");
+        console.log(blackCells);
+        for (var index = 0; index < blackCells.length; index++) {
+            var cell = blackCells[index];
+            while (cell.firstChild) {
+                cell.removeChild(cell.firstChild);
+            }
+            cell.removeEventListener("click", moveSelectedCheckerHere);
+        }
+    }
+}
+
+function isThereAForcedKillOnBoard() {
+    console.log(numOfKillInThisTurn, " =======numOfKillInThisTurn")
+    if (!(anotherdKillForThisTurn)) {
+        var allCheckers = checkers;
+        for (var i = 0; i < allCheckers.length; i++) {
+            if (allCheckers[i].color != turn) {
+                var checker = allCheckers[i];
+
+                for (var j = 0; j < allCheckers.length; j++) {
+                    if (allCheckers[j].color == turn) {
+                        var opponentChecker = allCheckers[j];
+                        if (checker.color == "white" || checker.isKing) {
+                            var emptyCell = document.getElementById("cell-" + (checker.row + 2) + "-" + (checker.cell + 2));
+                            if (((checker.row + 1 == opponentChecker.row) && (checker.cell + 1 == opponentChecker.cell)) &&
+                                (emptyCell != null && emptyCell.children.length <= 0)) {
+                                var checkerDiv = document.getElementById("checker-" + i)
+                                checkerDiv.classList.add("ready-to-kill");
+                                console.log("There is A Forced Kill on row" + checker.row + " " + checker.cell)
+                                forcedKillOnBoard = true;
+                            }
+                            emptyCell = document.getElementById("cell-" + (checker.row + 2) + "-" + (checker.cell - 2));
+                            if (((checker.row + 1 == opponentChecker.row) && (checker.cell - 1 == opponentChecker.cell)) &&
+                                (emptyCell != null && emptyCell.children.length <= 0)) {
+                                var checkerDiv = document.getElementById("checker-" + i)
+                                checkerDiv.classList.add("ready-to-kill");
+                                console.log("There is A Forced Kill on row" + checker.row + " " + checker.cell)
+                                forcedKillOnBoard = true;
+                            }
+                        }
+
+                        if (checker.color == "black" || checker.isKing) {
+                            var emptyCell = document.getElementById("cell-" + (checker.row - 2) + "-" + (checker.cell + 2));
+                            if (((checker.row - 1 == opponentChecker.row) && (checker.cell + 1 == opponentChecker.cell)) &&
+                                (emptyCell != null && emptyCell.children.length <= 0)) {
+                                var checkerDiv = document.getElementById("checker-" + i)
+                                checkerDiv.classList.add("ready-to-kill");
+                                console.log("There is A Forced Kill on row" + checker.row + " " + checker.cell)
+                                forcedKillOnBoard = true;
+                            }
+                            emptyCell = document.getElementById("cell-" + (checker.row - 2) + "-" + (checker.cell - 2));
+                            if (((checker.row - 1 == opponentChecker.row) && (checker.cell - 1 == opponentChecker.cell)) &&
+                                (emptyCell != null && emptyCell.children.length <= 0)) {
+                                var checkerDiv = document.getElementById("checker-" + i)
+                                checkerDiv.classList.add("ready-to-kill");
+                                console.log("There is A Forced Kill on row" + checker.row + " " + checker.cell)
+                                forcedKillOnBoard = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return forcedKillOnBoard;
+}
+function isThereAnotherForcedKillForThisTurn(checker) {
+
+    console.log(numOfKillInThisTurn, " =======numOfKillInThisTurn")
+    anotherdKillForThisTurn = false;
+    var allCheckers = checkers;
+
+    console.log("checker.color!!!!!", checker.color)
+    for (var j = 0; j < allCheckers.length; j++) {
+        if (allCheckers[j].color != turn) {
+            console.log("ppppppppppp", allCheckers[j])
+            var opponentChecker = allCheckers[j];
+
+            if (checker.color == "white" || checker.isKing || numOfKillInThisTurn > 0) {
+                console.log("in function isThereAnotherForcedKillForThisTurn")
+                var emptyCell = document.getElementById("cell-" + (checker.row + 2) + "-" + (checker.cell + 2));
+                if (((checker.row + 1 == opponentChecker.row) && (checker.cell + 1 == opponentChecker.cell)) &&
+                    (emptyCell != null && emptyCell.children.length <= 0)) {
+                    var checkerDiv = document.getElementById("cell-" + (checker.row) + "-" + (checker.cell)).firstChild;
+                    checkerDiv.classList.add("ready-to-kill");
+                    // checkerDiv.classList.add("selected");
+                    console.log("There is A Forced Kill on row" + checker.row + " " + checker.cell)
+                    anotherdKillForThisTurn = true;
+                }
+                emptyCell = document.getElementById("cell-" + (checker.row + 2) + "-" + (checker.cell - 2));
+                if (((checker.row + 1 == opponentChecker.row) && (checker.cell - 1 == opponentChecker.cell)) &&
+                    (emptyCell != null && emptyCell.children.length <= 0)) {
+                    var checkerDiv = document.getElementById("cell-" + (checker.row) + "-" + (checker.cell)).firstChild;
+                    checkerDiv.classList.add("ready-to-kill");
+                    console.log("There is A Forced Kill on row" + checker.row + " " + checker.cell)
+                    anotherdKillForThisTurn = true;
+                }
+            }
+
+            if (checker.color == "black" || checker.isKing || numOfKillInThisTurn > 0) {
+                console.log("There is A Forced Kill on row" + checker.row + " " + checker.cell)
+
+                var emptyCell = document.getElementById("cell-" + (checker.row - 2) + "-" + (checker.cell + 2));
+                if (((checker.row - 1 == opponentChecker.row) && (checker.cell + 1 == opponentChecker.cell)) &&
+                    (emptyCell != null && emptyCell.children.length <= 0)) {
+                    var checkerDiv = document.getElementById("cell-" + (checker.row) + "-" + (checker.cell)).firstChild;
+                    checkerDiv.classList.add("ready-to-kill");
+                    console.log("There is A Forced Kill on row" + checker.row + " " + checker.cell)
+                    anotherdKillForThisTurn = true;
+                }
+                emptyCell = document.getElementById("cell-" + (checker.row - 2) + "-" + (checker.cell - 2));
+                if (((checker.row - 1 == opponentChecker.row) && (checker.cell - 1 == opponentChecker.cell)) &&
+                    (emptyCell != null && emptyCell.children.length <= 0)) {
+                    var checkerDiv = document.getElementById("cell-" + (checker.row) + "-" + (checker.cell)).firstChild;
+                    checkerDiv.classList.add("ready-to-kill");
+                    console.log("There is A Forced Kill on row" + checker.row + " " + checker.cell)
+                    anotherdKillForThisTurn = true;
+                }
+            }
+        }
+    }
+    return anotherdKillForThisTurn;
+}
+
+
+function isThereAWin(checkers, turn) {
+    opponentOutOfCheckers(checkers);
+    opponentCannotMoveWin(turn);
+}
+
+
+function opponentOutOfCheckers(checkers) {
+    var numOfBlackCheckers = 0;
+    var numOfWhiteCheckers = 0;
+    for (let index = 0; index < checkers.length; index++) {
+        var checker = checkers[index];
+
+        if (checker.color == "black") {
+            numOfBlackCheckers++;
+            console.log(numOfBlackCheckers)
+        }
+        if (checker.color == "white") {
+            numOfWhiteCheckers++;
+            console.log(numOfWhiteCheckers)
+        }
+    }
+    if (numOfBlackCheckers == 0) {
+        clearBoard();
+        alert("GAME OVER: WHITE PLYAR WON")
+    }
+    if (numOfWhiteCheckers == 0) {
+        clearBoard();
+        alert("GAME OVER: BLACK PLYAR WON")
+    }
+}
+function opponentCannotMoveWin(turn) {
+    var win = true;
+    var color = turn == "white" ? "black" : "white";
+    var allBlackCells = document.getElementsByClassName("cell black");
+    var allOpponentCheckers = document.getElementsByClassName(color + "-checker");
+    for (let index = 0; index < allOpponentCheckers.length; index++) {
+        var checkerDiv = allOpponentCheckers[index];
+        var i = checkerDiv.id.split("-")[1];
+        var checker = checkers[i];
+        console.log("checker =======", checker)
+        for (let j = 0; j < allBlackCells.length; j++) {
+            var cell = allBlackCells[j];
+            if (isALegalMove(checker, cell)) {
+                killedCheckersPositionInArray = undefined;
+                win = false;
+                break;
+            }
+        }
+
+    }
+    if (win) {
+        alert("GAME OVER: " + turn + " PLYAR WON," + color + "Cannot Move")
+        setTimeout(function () { clearBoard(); }, 4000)
+    }
+    console.log("win ===", win)
+    console.log("turn====================================", turn)
+    return win;
+}
+function isDraw(numOfKingsMoveWithoutKill) {
+    if (numOfKingsMoveWithoutKill > 5) {
+        alert("DRAW: GAME OVER")
+    }
+}
+
---
+++ DamkaProject/checkers.js
@@ -0,0 +1,233 @@
+var selectedChecker = undefined;
+var killedCheckersPositionInArray = undefined;
+var turn = "white";
+var checkers = [
+    { row: 1, cell: 2, color: 'white', isKing: false },
+    { row: 1, cell: 4, color: 'white', isKing: false },
+    { row: 1, cell: 6, color: 'white', isKing: false },
+    { row: 1, cell: 8, color: 'white', isKing: false },
+    { row: 2, cell: 1, color: 'white', isKing: false },
+    { row: 2, cell: 3, color: 'white', isKing: false },
+    { row: 2, cell: 5, color: 'white', isKing: false },
+    { row: 2, cell: 7, color: 'white', isKing: false },
+    { row: 3, cell: 2, color: 'white', isKing: false },
+    { row: 3, cell: 4, color: 'white', isKing: false },
+    { row: 3, cell: 6, color: 'white', isKing: false },
+    { row: 3, cell: 8, color: 'white', isKing: false },
+
+
+
+    { row: 6, cell: 1, color: 'black', isKing: false },
+    { row: 6, cell: 3, color: 'black', isKing: false },
+    { row: 6, cell: 5, color: 'black', isKing: false },
+    { row: 6, cell: 7, color: 'black', isKing: false },
+    { row: 7, cell: 2, color: 'black', isKing: false },
+    { row: 7, cell: 4, color: 'black', isKing: false },
+    { row: 7, cell: 6, color: 'black', isKing: false },
+    { row: 7, cell: 8, color: 'black', isKing: false },
+    { row: 8, cell: 1, color: 'black', isKing: false },
+    { row: 8, cell: 3, color: 'black', isKing: false },
+    { row: 8, cell: 5, color: 'black', isKing: false },
+    { row: 8, cell: 7, color: 'black', isKing: false },
+
+];
+var firstBoard = true;
+function renderCheckers() {
+    console.log('rendering checkers', checkers);
+    clearBoard(firstBoard);
+    for (var i = 0; i < checkers.length; i++) {
+        var checker = checkers[i];
+        console.log(checker);
+        var cell = document.getElementById("cell-" + checker.row + '-' + checker.cell);
+        cell.appendChild(renderChecker(i, checker.color));
+    }
+
+    // $('.checker').click(selectChecker);
+    if (!firstBoard) {
+        isThereAWin(checkers, turn);
+    }
+
+    firstBoard = false;
+}
+
+
+function renderChecker(i, color) {
+    var checker = document.createElement("div");
+    checker.id = "checker-" + i;
+    checker.className = "checker " + color + "-checker crown";
+    checker.checerPosition = i;
+    var crownImg = document.createElement("img");
+
+
+    if (checkers[i].isKing) {
+        crownImg.className = "crown-img";
+        crownImg.src = "images/crown.png";
+        checker.appendChild(crownImg);
+    }
+
+    // checker.addEventListener("dragstart",selectChecker,false)
+    // checker.draggable="true";
+    // checker.ondragstart=drag(event);
+    readyTokillNextMove = forcedKillOnBoard;
+    checker.addEventListener("click", selectChecker);
+    console.log("****************************** ", typeof (checker));
+    forcedKillOnBoard = false;
+    return checker;
+}
+
+
+
+function selectChecker() {
+    if (readyTokillNextMove) {
+        if (this.classList.contains("selected")) {
+            console.log(`this checker was already selected`)
+            this.classList.remove("selected")
+            return
+        }
+        priviosSelectedChecker = document.getElementsByClassName("selected")[0];
+        console.log("priviosSelectedChecker ==", priviosSelectedChecker);
+        if (priviosSelectedChecker != undefined) {
+            priviosSelectedChecker.classList.remove("selected");
+        }
+        var checkerIndex = this.checerPosition;
+        if (checkers[checkerIndex].color == turn && this.classList.contains("ready-to-kill")) {
+            console.log("checkerIndex == ", this.checerPosition);
+            selectedChecker = checkers[checkerIndex];
+            this.classList.add("selected");
+
+            madeCellResponsive();
+        }
+    } else {
+        if (this.classList.contains("selected")) {
+            console.log(`this checker was already selected`)
+            // this.classList.remove("selected")
+            return
+        }
+        //     console.log(this);
+        priviosSelectedChecker = document.getElementsByClassName("selected")[0];
+        console.log("priviosSelectedChecker ==", priviosSelectedChecker);
+        if (priviosSelectedChecker != undefined) {
+            priviosSelectedChecker.classList.remove("selected");
+        }
+        var checkerIndex = this.checerPosition;
+        if (checkers[checkerIndex].color == turn) {
+            selectedChecker = checkers[checkerIndex];
+            console.log(`Finished selecting checker: `, selectedChecker)
+            this.classList.add("selected");
+            madeCellResponsive();
+
+        }
+    }
+}
+
+
+function isALegalMove(checker, blackCell) {
+    var isLegalMove = false;
+    var id = blackCell.id;
+    var idParts = id.split('-')
+    checkerRow = checker.row;
+    checkerCell = checker.cell;
+
+    cellRow = Number(idParts[1])
+    cellColumn = Number(idParts[2])
+
+    if (blackCell.children.length <= 0) {
+
+        if (checker.color == "black" || checker.isKing || numOfKillInThisTurn > 0) {
+            if ((!(forcedKillOnBoard)) && (cellRow == checkerRow - 1) && (cellColumn == checkerCell + 1 || cellColumn == checkerCell - 1)) {
+                isLegalMove = true;
+            }
+            if ((cellRow == checkerRow - 2) && (cellColumn == checkerCell + 2)) {
+                var skipCell = document.getElementById("cell-" + (checker.row - 1) + "-" + (checker.cell + 1));
+                if (skipCell.children.length > 0 && !(skipCell.classList.contains("black-checker"))) {
+                    for (var index = 0; index < checkers.length; index++) {
+                        var c = checkers[index];
+                        if (c.row == checker.row - 1 && c.cell == checker.cell + 1 && c.color != checker.color) {
+                            killedCheckersPositionInArray = index;
+                            isLegalMove = true;
+
+                            break;
+                        }
+                    }
+                }
+            }
+            if ((cellRow == checkerRow - 2) && (cellColumn == checkerCell - 2)) {
+                var skipCell = document.getElementById("cell-" + (checker.row - 1) + "-" + (checker.cell - 1));
+                if (skipCell.children.length > 0 && !(skipCell.classList.contains("black-checker"))) {
+                    for (var index = 0; index < checkers.length; index++) {
+                        var c = checkers[index];
+                        if (c.row == checker.row - 1 && c.cell == checker.cell - 1 && c.color != checker.color) {
+                            killedCheckersPositionInArray = index;
+                            // checkers.splice(index, 1);
+                            isLegalMove = true;
+                            console.log(isLegalMove, "3333333333333333333333333###########")
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (checker.color == "white" || checker.isKing || numOfKillInThisTurn > 0) {
+            console.log("2")
+            if ((!(forcedKillOnBoard)) && (cellRow == checkerRow + 1) && (cellColumn == checkerCell + 1 || cellColumn == checkerCell - 1)) {
+                // selectedChecker.row = cellRow;
+                // selectedChecker.cell = cellColumn;
+                console.log("3", checker.color, " ", cellRow, " ", checkerRow)
+                console.log(checker.color == "white")
+                console.log(numOfKillInThisTurn > 0)
+                console.log(checker.isKing)
+                isLegalMove = true;
+            }
+            if ((cellRow == checkerRow + 2) && (cellColumn == checkerCell + 2)) {
+                var skipCell = document.getElementById("cell-" + (checker.row + 1) + "-" + (checker.cell + 1));
+                if (skipCell.children.length > 0 && !(skipCell.classList.contains("white-checker"))) {
+                    for (var index = 0; index < checkers.length; index++) {
+                        var c = checkers[index];
+                        if (c.row == checker.row + 1 && c.cell == checker.cell + 1 && c.color != checker.color) {
+                            killedCheckersPositionInArray = index;
+                            // checkers.splice(index, 1);
+                            isLegalMove = true;
+                            break;
+                        }
+                    }
+                }
+
+            }
+            if ((cellRow == checkerRow + 2) && (cellColumn == checkerCell - 2)) {
+                var skipCell = document.getElementById("cell-" + (checker.row + 1) + "-" + (checker.cell - 1));
+                if (skipCell.children.length > 0 && !(skipCell.classList.contains("white-checker"))) {
+                    for (var index = 0; index < checkers.length; index++) {
+                        var c = checkers[index];
+                        if (c.row == checker.row + 1 && c.cell == checker.cell - 1 && c.color != checker.color) {
+                            killedCheckersPositionInArray = index;
+                            // checkers.splice(index, 1);
+                            isLegalMove = true;
+                            break;
+                        }
+                    }
+
+                }
+
+            }
+        }
+    }
+    console.log(`isLegalMove == `, isLegalMove)
+    return isLegalMove;
+}
+
+
+function madeCellResponsive() {
+    var blackCells = document.getElementsByClassName("cell black");
+    console.log(blackCells.length);
+    for (var i = 0; i < blackCells.length; i++) {
+        var cell = blackCells[i];
+        if (cell.children.length <= 0) {
+            var allCheckersDivs = document.getElementsByClassName("selected")
+            if (allCheckersDivs.length > 0) {
+                // console.log(cell);
+                cell.addEventListener("click", moveSelectedCheckerHere);
+            }
+        }
+    }
+}
